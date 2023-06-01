45e1391472d0bc7dbe0f7423a6be9d2e390ec45d
Siarhei Ivashka <s.ivashka@andersenlab.com>
2023-06-01 14:11:50 +0300

    Add project files.

---
+++ Commands.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UpdateRVTParam
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Commands : IExternalDBApplication
+    {
+        readonly string OUTPUT_FILE = "OutputFile.rvt";
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+        {
+            DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+        {
+            LogTrace("Design Automation Ready event triggered...");
+            e.Succeeded = true;
+            EditWindowParametersMethod(e.DesignAutomationData.RevitDoc);
+        }
+
+        private void EditWindowParametersMethod(Document doc)
+        {
+            InputParams inputParams = JsonConvert.DeserializeObject<InputParams>(File.ReadAllText("papams.json"));
+
+            using (Transaction trans = new Transaction(doc))
+            {
+                trans.Start("Update window parameters");
+
+                FilteredElementCollector WindowCollector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType();
+                IList<ElementId> windowIds = WindowCollector.ToElementIds() as IList<ElementId>;
+
+                foreach (ElementId windowId in windowIds)
+                {
+                    Element Window = doc.GetElement(windowId);
+                    FamilyInstance FamInst = Window as FamilyInstance;
+                    FamilySymbol FamSym = FamInst.Symbol;
+                    SetElementParameter(FamSym, BuiltInParameter.WINDOW_HEIGHT, inputParams.Height);
+                    SetElementParameter(FamSym, BuiltInParameter.WINDOW_WIDTH, inputParams.Width);
+                }
+
+                trans.Commit();
+            }
+
+            //Saving the updated file
+            ModelPath ProjectModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(OUTPUT_FILE);
+            SaveAsOptions SAO = new SaveAsOptions();
+            SAO.OverwriteExistingFile = true;
+
+            LogTrace("Saving file...");
+            doc.SaveAs(ProjectModelPath, SAO);
+        }
+
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+        {
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        private void SetElementParameter(FamilySymbol FamSym, BuiltInParameter paraMeter, double parameterValue)
+        {
+            FamSym.get_Parameter(paraMeter).Set(parameterValue);
+        }
+
+        public class InputParams
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+        }
+
+        /// <summary>
+        /// This will appear on the Design Automation output
+        /// </summary>
+        private static void LogTrace(string format, params object[] args)
+        {
+            System.Console.WriteLine(format, args);
+        }
+    }
+}
